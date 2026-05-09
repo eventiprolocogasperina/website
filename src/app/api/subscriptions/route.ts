7069ff9543a8f1d41e95367f39d7d94d641b8d52
eventiprolocogasperina <eventi.prolocogasperina@gmail.com>
2026-05-09 12:43:47 +0200

    Test: cambio destinatario a gmail per test spoofing

--- src/app/api/subscriptions/route.ts
+++ src/app/api/subscriptions/route.ts
@@ -50,7 +50,7 @@ export async function POST(request: Request) {
 
     const data = await resend.emails.send({
       from: 'Pro Loco Gasperina <noreply@prolocogasperina.it>',
-      to: 'iscrizioni@prolocogasperina.it',
+      to: 'prolocogasperina@gmail.com',
       subject: `Nuova Iscrizione: ${nome} ${cognome}`,
       replyTo: email,
       html: htmlContent,
