7eee4fa338820c94e06feac15e3c0153242f58c0
eventi-proloco-gasperina <eventi.prolocogasperina@gmail.com>
2026-07-11 15:19:47 +0200

    style: move Assaggia e Passeggia banner above Upcoming Events on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,8 @@ export default async function HomePage() {
   return (
     <>
       <Hero />
-      <UpcomingEvents events={events} />
       <AssaggiaTeaser />
+      <UpcomingEvents events={events} />
       <DiscoverTeaser />
       <CommunityImpact />
     </>
